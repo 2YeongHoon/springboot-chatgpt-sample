554c86ad21d919de952e46c43542bb6fdb9c2233
2Yeonghoon <djaakclsrn12@naver.com>
2024-02-17 23:22:22 +0900

    NEW: 에러코드 정의 enum 추가

---
+++ src/main/java/external/chatgpt/enums/GptErrorCode.java
@@ -0,0 +1,17 @@
+package external.chatgpt.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum GptErrorCode {
+  FAIL_TO_CONVERT_MESSAGE(1001, "프롬포트 변환 중 오류가 발생했습니다."),
+
+  EXTERNAL_API_ERROR(1, "요청을 처리하던 중 예상하지 못한 오류가 발생했습니다."),
+  INTERNAL_SERVER_ERROR(9999, "요청을 처리하던 중 예상하지 못한 오류가 발생했습니다."),
+  ;
+
+  private final int code;
+  private final String message;
+}
